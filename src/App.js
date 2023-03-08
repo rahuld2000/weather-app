7c78b3b934f44721b16bfdeb8b73d6d7cc6db7ce
Rahul Dhiman <rahuldhiman.rd14@gmail.com>
2023-03-09 01:27:10 +0530

    files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+const Weather = () => {
+    const [city, setCity] = React.useState(null);
+    const[other,setOther]=React.useState(null);
+    const [search, setSearch] = React.useState("");
+    const [place, setPlace] = React.useState("New York");
+    React.useEffect(() => {
+        const api = async () => {
+            const Url = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=f61848ab5fa06141797d0d033e36ad85`;
+            const response = await fetch(Url);
+            const data = await response.json();
+            setCity(data.main);
+            setOther(data);
+            console.log(data);
+            
+        };
+        api();
+    }, [place]);
+    function handleClick(e) {
+        e.preventDefault();
+        setPlace(search)
+        setSearch("")
+    }
+    function handlePress(event) {
+        if (event.key === "Enter") {
+            console.log("press")
+            setPlace(search);
+            setSearch("")
+        }
+    }
+    return (
+        <div className="container-bg">
+            <form className="search-box" onSubmit={handlePress}>
+                <input type="search" value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                <button onClick={handleClick}>search</button>
+            </form>
+        
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+            { !city ? (
+            <p>No Data Found</p>) : (<div className="weather-info">
+            <div className="container">
+                <div className="cont_1">
+                  <div className="temp"><span className="temp-no">{city.temp}</span> <span className="temp-unit">°C</span></div>
+                  <img src={`http://openweathermap.org/img/wn/${other.weather[0].icon}@2x.png`} alt="error" />
+                  </div>
+                    <p> feel like {city.feels_like}</p>
+                    <p>weather  {other.weather[0].description}</p>
+                    <p>name {other.name}</p>
+                    <span>country {other.sys.country}</span> 
+                    
+             
+                </div>
+                </div>) }
+
+        </div>
+    )
 }
-
-export default App;
+export default Weather;
